e446239e025cf6817b0664a852e1bf70ac7c6b69
602116 <602116@stud.hvl.no>
2022-01-27 13:44:04 +0100

    Gjort klart for Andre

--- src/no/hvl/dat102/Filmarkiv2.java
+++ src/no/hvl/dat102/Filmarkiv2.java
@@ -4,28 +4,59 @@ import no.hvl.dat102.adt.FilmarkivADT;
 
 import java.util.Arrays;
 
-public class Filmarkiv2 implements FilmarkivADT {
+public class Filmarkiv2 implements FilmarkivADT{
+    private int antall;
+    private LinearNode<Film> start;
+    // Ingen referanse til siste, kun start
+    ...
+    /*  Klassen skal ha de samme operasjoner som for Filmarkiv i
+        øving 2, men pass på at implementeringen av alle metoder
+        blir tilpasset den nye strukturen. */
 
 
 
 
-    public Filmarkiv2(int antall) {
+
+    public Filmarkiv2() {
+        this.antall=0;
 
     }
 
     private Film[] utvid(Film[] tmp) {
 
-
+    return null;
 
     }
 
     @Override
     public Film finnFilm(int nr) {
+        LinearNode<Film> tmp=start;
+
+
+        for (int i = 0; i < antall(); i++) {
+            Film tmpFilm = tmp.getElement();
+
+            if (tmpFilm.getFilmnr()==nr)return tmpFilm;
+
+            tmp=tmp.getNeste();
+
+        }
+        return null;
 
     }
 
     @Override
     public void leggTilFilm(Film nyFilm) {
+        if (antall==0) {
+
+            start.setElement(nyFilm);
+        }else{
+            LinearNode<Film>neste = null;
+            neste.setElement(nyFilm);
+            start.setNeste(neste);
+        }
+
+        antall++;
 
     }
 
